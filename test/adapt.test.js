c948837231c3c28fd39944790c0d2bc2d0132438
zhaoyao91 <zhaoyao91@163.com>
2018-02-12 23:56:26 +0800

    add test for adapt

--- test/adapt.test.js
+++ test/adapt.test.js
@@ -1,7 +1,18 @@
-const {adapt} = require('../index')
+const fetch = require('node-fetch')
+const http = require('http')
+const {adapt, Response} = require('../index')
 
 describe('adapt', () => {
-  it('should create a typical http server', () => {
-
+  it('should create a typical http server', async () => {
+    const port = 3000
+    const url = `http://localhost:${port}`
+    const handler = (request) => Response.withTextBody('Hello World')
+    const httpHandler = adapt(handler)
+    const server = http.createServer(httpHandler)
+    server.listen(port)
+    const response = await fetch(url)
+    const body = await response.text()
+    expect(body).toBe('Hello World')
+    server.close()
   })
 })
