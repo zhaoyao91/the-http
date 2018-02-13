fbd5a7fe4a692c893fdb05929afb68621abcbe04
zhaoyao91 <zhaoyao91@163.com>
2018-02-13 22:33:12 +0800

    add test for build-upward-wrapper

---
+++ test/build-upward-wrapper.test.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch')
+const {listen, Response, buildUpwardWrapper} = require('../index')
+
+describe('buildDownwardWrapper', () => {
+  let server = null
+
+  afterEach(() => {
+    server.close()
+    server = null
+  })
+
+  it('should return a new response based on the previous one', async () => {
+    const port = 3000
+    const url = `http://localhost:${port}`
+    const handler = (request) => Response.withTextBody('Hello')
+    const wrapper = buildUpwardWrapper((response) => {
+      return Response.withTextBody(response.body + ' World')
+    })
+    server = listen(port)(wrapper(handler))
+    const response = await fetch(url)
+    const body = await response.text()
+    expect(body).toBe('Hello World')
+  })
+})
